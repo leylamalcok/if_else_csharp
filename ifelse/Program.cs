64cfb72c380050def380afbf5747e2cccc63f6ef
gizemgizg <lyl.mlck0693@gmail.com>
2021-06-09 22:31:23 +0300

    if else cmmt

---
+++ ifelse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yas;
+
+            Console.WriteLine("Yaşınızı Giriniz:");
+            Convert.ToInt32(Console.ReadLine());
+
+            if (yas < 18)
+                Console.WriteLine("Yaşınız 18 den küçük ehliyet alamazsınız.")
+
+                    else
+                Console.WriteLine("Ehliyet alabilirdiniz.");
+
+
+        }
+    }
+}
